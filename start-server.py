9c67889e0a83401eba46a451c1348ebe22e632ff
devonmurphy <devonmurphy1@gmail.com>
2016-11-27 22:28:59 +0000

    added files to install and start-server

--- start-server.py
+++ start-server.py
@@ -7,8 +7,8 @@ import json
 import smtplib
 
 ############################################
-EMAIL = "YOUR_GMAIL_ACCOUNT"
-PASS = "GMAIL_ACCOUNT_PASSWORD"
+EMAIL = "YOUR_GMAIL_EMAIL"
+PASS = "YOUR_GMAIL_PASSWORD"
 WEBSITE_USERNAME = "username"
 WEBSITE_PASSWORD = "password"
 ############################################
@@ -16,16 +16,16 @@ WEBSITE_PASSWORD = "password"
 directory = os.path.dirname(os.path.realpath(__file__))
 print directory
 
-time.sleep(5)
-os.system(directory + '/ngrok http 5000'+' -auth="'+WEBSITE_USERNAME+':'+WEBSITE_PASSWORD+'" -log=stdout > /dev/null &')
-os.system('python ' + directory + '/website.py &')
+time.sleep(10)
+os.system(directory + '/ngrok http 80'+' -auth="'+WEBSITE_USERNAME+':'+WEBSITE_PASSWORD+'" -log=stdout > /dev/null &')
+#os.system('python ' + directory + '/website.py &')
 time.sleep(5)
 r = requests.get('http://localhost:4040/api/tunnels/command_line')
 rJson = r.json()
 publicURL = rJson['public_url']
 
 stringPublicURL = str(publicURL)
-stringPublicURL = stringPublicURL[8:len(publicURL)]
+stringPublicURL = stringPublicURL[8:len(publicURL)]+"/zm"
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
