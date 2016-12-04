e930b80255f1827f7ff6492dcfa161625ab50207
Devon Murphy <devonmurphy1@gmail.com>
2016-12-04 10:35:18 +0000

    star-server updated for rpi cam

--- start-server.py
+++ start-server.py
@@ -9,17 +9,14 @@ import smtplib
 ############################################
 EMAIL = "YOUR_GMAIL_ACCOUNT"
 PASS = "GMAIL_PASSWORD"
-WEBSITE_USERNAME = "username"
-WEBSITE_PASSWORD = "password"
 ############################################
 
 directory = os.path.dirname(os.path.realpath(__file__))
 print directory
 
 time.sleep(10)
-os.system(directory + '/ngrok http 8080'+' -auth="'+WEBSITE_USERNAME+':'+WEBSITE_PASSWORD+'" -log=stdout > /dev/null &')
+os.system(directory + '/ngrok http 80 -log=stdout > /dev/null &')
 time.sleep(2)
-os.system('sudo -u pi '+directory + '/vlc-start-stream &')
 r = requests.get('http://localhost:4040/api/tunnels/command_line')
 rJson = r.json()
 publicURL = rJson['public_url']
@@ -31,5 +28,5 @@ server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(EMAIL, PASS)
 
-server.sendmail(EMAIL, EMAIL, '\nhttp://'+stringPublicURL+'/stream')
+server.sendmail(EMAIL, EMAIL, '\nhttp://'+stringPublicURL+'/html')
 server.quit()
